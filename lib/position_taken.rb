92ee421da6398d0177d7565070648c0df8d4aa54
Cookie McCormick <muntananuchat@gmail.com>
2018-09-29 15:27:20 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,3 @@
 def position_taken?(board, index)
   board[index] != " " && board[index] != "" && board[index] != nil
 end
-
-# code your #position_taken? method here!
-# def position_taken?(board, index)
-#   if board[index] == " " || board[index] == "" || board[index] == nil
-#     false
-#   else 
-#     true
-#   end
-# end
