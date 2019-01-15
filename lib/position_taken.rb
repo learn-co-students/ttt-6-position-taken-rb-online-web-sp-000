b662ad3e74f2109d7eb1b5adc0483d097d1f8c8a
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-01-15 07:12:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,9 @@
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken? == true
+else index.between?(0,8) || position_taken? == nil
+end
+end
+
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     true
