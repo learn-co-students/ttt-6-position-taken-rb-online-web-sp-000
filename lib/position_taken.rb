65ceff5231decc2a09cc38f71be750a11c64b06e
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-02-06 01:13:55 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+
+if board[0] == "X" || board[8] == "O"
+  true
+
+elsif board[2] == " "
+    false
+else
+  false
+end
+end
