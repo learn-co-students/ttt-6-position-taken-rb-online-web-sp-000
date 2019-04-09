83131fd110f8d4f9245a9709d1be41d853f50feb
ehatlebe <github email address>
2019-04-08 18:00:46 -0700

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
-# code your #position_taken? method here!
+# method validates input against current state of board
+# => important b/c game would break if plays could be overwritten with "X" or "O"
+# checks if a index position on board is empty (false) or has been taken (true)
+def position_taken?(board, index)
+  (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
+end
+
+# def position_taken?(board, index)
+#   if board[index] == " " || board[index] == "" || board[index] == nil
+#     false
+#   else
+#     true
+#   end
+# end
