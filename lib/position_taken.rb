e949ea9897629f862273d6b474ceffd5503bdd07
Sean Barile <aquariussb84@gmail.com>
2019-05-04 22:05:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,9 @@
 def position_taken?(board, index)
-  board[index] == " "
+taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  taken = false
+  else
+  taken = true
+  end
+  taken
 end
