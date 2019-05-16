9d99e25efea9dfaf3efd1ae36cd41985aa47cf99
Connor Cowan <ccowan003@comcast.net>
2019-05-16 00:16:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
 
-# We're accessing our array with board[index]
-taken = nil #initializing variable to nil
-if (board[index] == " " || board[index] == "" || board[index] == nil)
-  taken = false #updating variable to false, saying index is currently empty
-else
-  taken = true  #updating variable to index being true or "full"
+  # We're accessing our array with board[index]
+  taken = nil #initializing variable to nil
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    taken = false #updating variable to false, saying index is currently empty
+  else
+    taken = true  #updating variable to index being true or "full"
+  end
 end
