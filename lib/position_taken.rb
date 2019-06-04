e08fd4f4470e3bbbd5109db27e2f9c355205a152
github username <github email address>
2019-06-03 21:47:52 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  
+if (board[index] == " " || board[index] =="" || board[index] == nil)
+  # Don't need to wirte the word "return," but I put it to see how it work.
+    return false 
+  else
+    return true
+  end
+end
