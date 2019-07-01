4b5a3669c6cce8598cc7958543e26f3b7e4c6dd4
Alice Brunel <alc.brunel@gmail.com>
2019-07-01 19:38:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+board = [" ", " ", " ", " ", " ", " ", " ", "X", " "]
+
+def position_taken?(array,index)
+  if (array[index] == " ") || (array[index] == "")
+    false
+    elsif array[index] == nil
+    false
+  else
+    true
+  end
+end
