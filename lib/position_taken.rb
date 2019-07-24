79c4310c9911ecbdb33617e820d5919a903ef268
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-07-24 22:14:51 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index(X,O))
+  board = []
+  index = 0
+  if board == "" || " "
+    return true
+  else 
+    return false
+end
+  
