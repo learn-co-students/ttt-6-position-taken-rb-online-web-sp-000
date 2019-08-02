fa784f69cefefa5bc5f20962f1704a925c6d8d1d
Panela Leung <leungpanela@gmail.com>
2019-08-02 00:51:13 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,4 @@
 # code your #position_taken? method here!
-#def position_taken?(board, index)
- #board[index] == " " || board[index] == "" || board[index] == nil
-  #return false
- #board[index] != " " || "" || nil
-#board[index] == "X" || board[index] == "O"
 
 def position_taken?(board, index)
  !(board[index] == " " || board[index] == "" || board[index] == nil)
