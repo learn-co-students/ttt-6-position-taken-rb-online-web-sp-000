2f67bfbee3aa7e5bbafa3174d9643b8293d97fd8
github username <github email address>
2019-09-03 14:24:41 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,4 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)#This method is responsible for evaluating the user's input againist the Tic Tac Toe board and checking to see whether or not that position is occupied.
+  return((board[index] ==  " " || board[index] == "" || board[index] == nil) ? false : true);
+end
