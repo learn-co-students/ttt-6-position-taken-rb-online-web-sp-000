70a6f69e9ca89f7ef758bc6276897d2752872274
David Cantoran <davidcantoran36@gmail.com>
2019-09-30 21:10:27 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, idx)
+  
+  taken = false
+  
+  if board[idx] == " " || board[idx] == "" || board[idx] == nil 
+    taken = false 
+  else 
+    taken = true 
+  end 
+  taken 
+end 
