8899bc6742384502456a2dfee753c1a64d15a8cc
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-01 01:22:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
-if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
- return false
- else
-   return true
- end
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+    return false
+  else
+    return true
+  end
 end
