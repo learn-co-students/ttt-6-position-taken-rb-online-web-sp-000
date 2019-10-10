8540839496fb57a03bfd01fe6062fbe6231d23fc
Sweet Van Loan <sweet@sweetvanloan.com>
2019-10-10 22:41:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if Array[index_number] == ""
+    puts FALSE
+  ifelse Array[index_number] == " "
+    puts FALSE
+  else 
+    puts TRUE
+  end 
