d8bb7acea53f6e27c329a311e16fad34b947d65e
Alethea Cary <aletheacary@gmail.com>
2019-11-13 13:40:55 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == NIL
+    FALSE
+  elsif board[index].strip.length == 0
+    FALSE
+  else
+    TRUE
+  end
+    
+end
