f453a90cc4a2d56a0c20dac5043169cbe8101138
James Lenahan <james.k.lenahan@gmail.com>
2019-11-17 19:17:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+def position_taken? (board, index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == NIL
+    false
+  else board[index] == "X" || "O"
+    true
+  end
+end
+
