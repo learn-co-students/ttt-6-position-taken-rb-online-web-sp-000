781dc92dbafd7e5b1a0326ec21a9ada375cb6f16
Doak Covington <doakcovington@protonmail.com>
2019-11-26 02:54:04 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+#Method that checks if element at index is currently filled
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
