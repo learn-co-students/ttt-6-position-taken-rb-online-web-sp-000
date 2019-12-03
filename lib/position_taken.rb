846b0dc598a24e73b7547523353011c8f509cca9
Dr. XyClo <sdcarmody@gmail.com>
2019-12-03 00:26:54 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,7 @@
 
 
 def position_taken?(board, index)
+#<<<<<<< HEAD
     if board[index] != " " && board[index] != "" && board[index] != nil
     true
     else
@@ -10,12 +11,30 @@ def position_taken?(board, index)
     end
 end
 
+=begin
+    if board[index] != " "
+    true
+    
+    else
+    false
+    
+   
+      
+      end
+
+
+
+>>>>>>> 3c44be77d3cc6a2907cd9a91cef5175ec8c8f040
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 1
 position_taken?(board, index)
 
+<<<<<<< HEAD
 
 
 # test
 
+=======
+=end
+#>>>>>>> 3c44be77d3cc6a2907cd9a91cef5175ec8c8f040
