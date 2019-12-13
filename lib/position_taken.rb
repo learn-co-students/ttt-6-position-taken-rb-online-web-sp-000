1859d1fac939ed50726e38f2d1f845958df94188
Amy Fong <amyhfong@gmail.com>
2019-12-13 20:36:54 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here! #test
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, num)
   if board[num] == " " || board[num] == "" || board[num] == nil
