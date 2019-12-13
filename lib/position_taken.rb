d8bd5a41c7fc712a1a7f0a2f4a380e7581b0c08a
Will Carter <will.carter@fergusllc.com>
2019-12-13 20:30:42 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+def position_taken? (board, index)
+  taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+  taken
+end
+
+board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+index = 0
+puts position_taken?(board, index)
