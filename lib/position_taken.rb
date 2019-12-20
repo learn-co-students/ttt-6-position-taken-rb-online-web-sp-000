e176b6febad1f6f9f01aa7bc7ab721636453b514
davisjustinw <davisjustinw@gmail.com>
2019-12-20 00:19:24 -0800

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here! nothing to do
 def position_taken?(board, index)
   if board[index] == ' ' || board[index] == '' || board[index] == nil
     return FALSE
