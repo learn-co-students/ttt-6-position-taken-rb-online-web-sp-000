5164d197607f1887ce0099ce3a94b34d9069b0bc
Mike White <michaeljwhite@gmail.com>
2020-01-04 23:19:16 -0800

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+    case board[index]
+    when "", " ", nil
+        return false
+    else
+        return true
+    end
+end
