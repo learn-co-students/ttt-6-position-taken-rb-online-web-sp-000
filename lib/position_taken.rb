2ca7f5696a8dd1700bca04054c871e79df224b6c
Scott Wilson <scottwilson1020@gmail.com>
2020-01-11 17:42:29 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  
+  if board[index] == "O" ||  board[index] == "X"
+    true
+ 
+  else board[index] == " " || board[index] == ""
+    false
+  
+  end
+end
