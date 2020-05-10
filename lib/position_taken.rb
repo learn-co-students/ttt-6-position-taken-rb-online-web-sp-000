dfa6d4276b93b0e82f2c7b5949bf96f8da856d0f
elsa chen <snowing222@gmail.com>
2020-05-10 10:35:01 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,17 @@
 # code your #position_taken? method here!
+=begin
+def position_taken?(board,index)
+  if board[index]==""||board[index]==" "||board[index]==nil
+    return false
+
+  else
+    return true
+
+  end
+end
+=end
+
+def position_taken?(board,index)
+  (board[index]==""||board[index]==" "||board[index]==nil)? false:true
+
+end
