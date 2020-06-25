62f2efd333e595f38971bee14ff7642454e70fc0
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-25 01:02:59 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,14 @@
 # code your #position_taken? method here!
 
-def position_taken?(board, index, current_player = “X” || “O”)
-	if board[index] != “ “ && “” do
-		false
-	else
-		true
+def position_taken?(board, index)
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+  false : true
 end
+
+# taken = nil
+ # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+ #   taken = false
+ # else
+ #   taken = true
+ # end
+ # taken
