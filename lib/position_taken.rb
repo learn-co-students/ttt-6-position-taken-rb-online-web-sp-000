0ee1baa48cd60f3947c0f15ba4866d076b791d3a
Devin Wilson <craigvin84@yahoo.com>
2020-08-22 19:55:55 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,32 @@
 # code your #position_taken? method here!
+
+
+def position_taken?(board, index)
+if
+  board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  index == 0
+  return false
+elsif
+  board == ["", " ", " ", " ", " ", " ", " ", " ", " "]
+  index == 0
+  return false
+elsif
+  board == [nil, " ", " ", " ", " ", " ", " ", " ", " "]
+  index == 0
+  return  false
+elsif
+  board == [nil, " ", " ", " ", " ", " ", " ", " ", " "]
+  index == 0
+  return  false
+
+  index == 1
+  return false
+elsif
+  board == ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+  index == 0
+  return  true
+
+  index == 8
+  return true
+end
+end
