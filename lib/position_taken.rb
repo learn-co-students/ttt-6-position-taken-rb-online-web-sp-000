cf42cc615ae4737d7f42ba8293c70d846543edcf
Khaled S Momani <khaledmomani4@gmail.com>
2020-09-08 01:28:35 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,22 @@
-# code your #position_taken? method here!
+def valid_move?(board,index)
+  if  inxdex.to_i.between?(1,9) && position_taken?
+    true
+  else
+     false
+  end
+end
+
+
+
+def position_taken?(board,position)
+  if board[position] == " "
+    false
+    elsif board[position] == ""
+    false
+    elsif  board[position] == nil
+    false
+    else   board[position] == "X" || "O"
+    true
+
+  end
+end
