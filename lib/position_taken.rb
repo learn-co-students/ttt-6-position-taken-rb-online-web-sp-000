e0f0ac76746fb443c79710c0131584616ddaac4d
Hannah Malm <hannahemalm616@gmail.com>
2020-10-08 01:38:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,22 @@
 # code your #position_taken? method here!
+
+
+# Our position_taken? method should return false if the submitted index is equal to either an empty string, " ", or an empty string "", or nil.
+
+# def position_taken?(board,index)
+#   if array[index_number] == " " || array[index_number] == "" ||  array[index_number] == nil
+#     "false"
+#   else array[index_number] == "X" || array[index_number] == "O"
+#     "true"
+#   end
+# end
+  
+
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+  
