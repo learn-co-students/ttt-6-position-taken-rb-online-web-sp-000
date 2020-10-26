53bb030cc2a9d14dd28d1f0001213bdb6a703ef7
sphinxx0000 <sphinxx0000@gmail.com>
2020-10-25 21:32:26 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if (board[0] == " ") || (board[0] == "" || (board[0] == nil))
+  false
+elsif (board[0] == "X") || (board[8] == "O")
+  !!board
+  end
+end
