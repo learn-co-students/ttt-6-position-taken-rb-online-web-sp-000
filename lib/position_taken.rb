414bfc4d0855e8ad046c55ddf928a049a860626b
Harrison Price <hprice518@gmail.com>
2020-10-31 23:41:14 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
-# code your #position_taken? method here!
+
+#def position_taken?(board, input)
+# if input != "X" || input != "O"
+#  return FALSE 
+#  else board[X] || board[O]
+#   return TRUE
+#  end
+#end
+
+def position_taken?(board, index)
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+    return false
+  else
+    return true  
+  end 
+end
