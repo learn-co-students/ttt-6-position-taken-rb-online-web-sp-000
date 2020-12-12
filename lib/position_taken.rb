1cd64b8477ab70f9447e8df56e869fbfe2323359
Shami Tomita <shamitomita@gmail.com>
2020-12-12 18:23:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,3 +3,5 @@
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
 end 
+
+#important to sometimes try to condense and simplify as much as you can, if ruby is flexible and neat you should try the path of least resistance 
