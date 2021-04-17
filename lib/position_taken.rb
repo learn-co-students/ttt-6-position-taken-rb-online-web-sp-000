8b3f4653a601534cc6a42e33c425bcdb3320bfb0
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-04-17 17:50:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@
 def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil 
     return false 
-  elsif board[index] == "X" || board[index] == "O"
+  else board[index] == "X" || board[index] == "O"
     return true 
   end 
 end 
