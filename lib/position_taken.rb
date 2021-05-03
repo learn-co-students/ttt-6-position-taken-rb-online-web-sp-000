7bb6b541c1002f8b7bfa92bb06bc586b2e4ae7bc
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-03 19:03:07 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board == "  " || " " || nil
-      false
-  else index == "X" || "O"
-      true
-  end
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false 
+   else (board[index] == "X") || (board[index] == "O")
+      return true
+   end
 end
